828ab6bb12f00b10c7d98f218790e72adaecf2a3
jaeusd <pcr1412@gmail.com>
2022-10-13 22:25:58 +1000

    Updates

---
+++ precache-manifest.24d42452176b39827492a9fe0d352f25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca6ec70c314a1f6c20d440f99d3d61e",
+    "url": "/Decentralised-Exchange/index.html"
+  },
+  {
+    "revision": "f18c664ce1cad0c38d61",
+    "url": "/Decentralised-Exchange/static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "be42dcb5386b3df7c373",
+    "url": "/Decentralised-Exchange/static/css/main.9c453fb2.chunk.css"
+  },
+  {
+    "revision": "f18c664ce1cad0c38d61",
+    "url": "/Decentralised-Exchange/static/js/2.de5bd28e.chunk.js"
+  },
+  {
+    "revision": "fb40d9e6366efed580d4198944612660",
+    "url": "/Decentralised-Exchange/static/js/2.de5bd28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be42dcb5386b3df7c373",
+    "url": "/Decentralised-Exchange/static/js/main.fbe4572a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Decentralised-Exchange/static/js/main.fbe4572a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be0540ad9d583b87c7e",
+    "url": "/Decentralised-Exchange/static/js/runtime-main.b738d710.js"
+  }
+]);
